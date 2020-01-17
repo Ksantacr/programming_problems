53a408b93df823b99b9796094f298a330037e11b
Kevin Santacruz <defonix0@gmail.com>
2020-01-16 23:36:03 -0500

    birthday cake candle

--- ProgrammingProblems/BirthdayCakeCandles.cs
+++ ProgrammingProblems/BirthdayCakeCandles.cs
@@ -1,37 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ConsoleApp1
 {
     public class BirthdayCakeCandles
     {
+        // https://www.hackerrank.com/challenges/birthday-cake-candles/problem
         public static int birthdayCakeCandles(int[] ar)
         {
             int tallest = 0;
-            int candlesCanBlow = 0;
-            for (int i = 0; i < ar.Length; i++)
-            {
-                for (int j = i + 1; j < ar.Length; j++)
-                {
-                    int tmp = ar[i];
-                    if (ar[i] < ar[j])
-                    {
-                        ar[i] = ar[j];
-                        ar[j] = tmp;
-                    }
-                }
-            }
+            Array.Sort(ar);
+            Array.Reverse(ar);
             tallest = ar[0];
-
-            for (int i = 0; i < ar.Length; i++)
-            {
-                if (ar[i] < tallest)
-                {
-                    candlesCanBlow++;
-                }
-            }
-            return candlesCanBlow;
+            return ar.Count(x => x == tallest);
         }
     }
 }
